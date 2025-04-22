689e130fcb8d5d87888e286e0cd24f55ba499f48
MaartenSmetsPXL <maarten.smets@student.pxl.be>
2025-04-22 15:25:08 +0200

    TableTest: Class_ShouldImplement_ITable en Constructor_ShouldInitializeProperties gemaakt

--- Backend/Azul.Core/TableAggregate/Table.cs
+++ Backend/Azul.Core/TableAggregate/Table.cs
@@ -1,4 +1,6 @@
 ﻿using System.Drawing;
+using System.Numerics;
+using System.Text;
 using Azul.Core.PlayerAggregate;
 using Azul.Core.PlayerAggregate.Contracts;
 using Azul.Core.TableAggregate.Contracts;
@@ -7,9 +9,42 @@ using Azul.Core.UserAggregate;
 namespace Azul.Core.TableAggregate;
 
 /// <inheritdoc cref="ITable"/>
-internal class Table
+internal class Table : ITable
 {
+
+
     internal Table(Guid id, ITablePreferences preferences)
     {
+        Id = id;
+        Preferences = preferences;
+
+        SeatedPlayers = new List<IPlayer>();
+
+
+    }
+
+    public Guid Id { get; set; }
+
+    public ITablePreferences Preferences { get; set; }
+
+    public IReadOnlyList<IPlayer> SeatedPlayers { get; set; }
+
+    public bool HasAvailableSeat => SeatedPlayers.Count < 4;
+
+    public Guid GameId { get; set;  }
+
+    public void FillWithArtificialPlayers(IGamePlayStrategy gamePlayStrategy)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Join(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Leave(Guid userId)
+    {
+        throw new NotImplementedException();
     }
 }
