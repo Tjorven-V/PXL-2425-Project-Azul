1379a6d2c5c29abfef9ce9c99c166c1867b65285
ismaelPXL <ismael.ahaddour@student.pxl.be>
2025-04-25 22:20:15 +0200

    implemented GameService

--- Backend/Azul.Core/GameAggregate/GameService.cs
+++ Backend/Azul.Core/GameAggregate/GameService.cs
@@ -1,6 +1,8 @@
 ﻿using Azul.Core.GameAggregate.Contracts;
 using Azul.Core.PlayerAggregate;
 using Azul.Core.PlayerAggregate.Contracts;
+using Azul.Core.TableAggregate.Contracts;
+using Azul.Core.TableAggregate;
 using Azul.Core.TileFactoryAggregate.Contracts;
 
 namespace Azul.Core.GameAggregate;
@@ -8,25 +10,33 @@ namespace Azul.Core.GameAggregate;
 /// <inheritdoc cref="IGameService"/>
 internal class GameService : IGameService
 {
+    private readonly IGameRepository _gameRepository;
+
     public GameService(IGameRepository gameRepository)
     {
+        _gameRepository = gameRepository;
     }
     public IGame GetGame(Guid gameId)
     {
-        throw new NotImplementedException();
+        // 1. Getting game from the repository
+        return _gameRepository.GetById(gameId);
+
     }
     public void TakeTilesFromFactory(Guid gameId, Guid playerId, Guid displayId, TileType tileType)
     {
-        throw new NotImplementedException();
+        var game = _gameRepository.GetById(gameId);
+        game.TakeTilesFromFactory(playerId, displayId, tileType);
     }
 
     public void PlaceTilesOnPatternLine(Guid gameId, Guid playerId, int patternLineIndex)
     {
-        throw new NotImplementedException();
+        var game = _gameRepository.GetById(gameId);
+        game.PlaceTilesOnPatternLine(playerId, patternLineIndex);
     }
 
     public void PlaceTilesOnFloorLine(Guid gameId, Guid playerId)
     {
-        throw new NotImplementedException();
+        var game = _gameRepository.GetById(gameId);
+        game.PlaceTilesOnFloorLine(playerId);
     }
 }
