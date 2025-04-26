8dd63ff1bf18498e8834efdb148f4d3745e54442
MaartenSmetsPXL <maarten.smets@student.pxl.be>
2025-04-26 11:53:51 +0200

    Playertests US3

--- Backend/Azul.Core/PlayerAggregate/HumanPlayer.cs
+++ Backend/Azul.Core/PlayerAggregate/HumanPlayer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Runtime.InteropServices.Marshalling;
 using Azul.Core.BoardAggregate.Contracts;
 using Azul.Core.PlayerAggregate.Contracts;
 using Azul.Core.TileFactoryAggregate.Contracts;
@@ -6,19 +7,9 @@ using Azul.Core.TileFactoryAggregate.Contracts;
 namespace Azul.Core.PlayerAggregate;
 
 /// <inheritdoc cref="IPlayer"/>
-internal class HumanPlayer : IPlayer
+internal class HumanPlayer : PlayerBase
 {
-    public Guid Id { get; }
-    public string Name { get; }
-    public DateOnly? LastVisitToPortugal { get; }
-    public IBoard Board { get; }
-    public bool HasStartingTile { get; set; }
-    public List<TileType> TilesToPlace { get; }
-
     public HumanPlayer(Guid userId, string name, DateOnly? lastVisitToPortugal)
-    {
-        Id = userId;
-        Name = name;
-        LastVisitToPortugal = lastVisitToPortugal;
-    }
+        :base(userId, name, lastVisitToPortugal)
+    {}
 }
--- Backend/Azul.Core/PlayerAggregate/PlayerBase.cs
+++ Backend/Azul.Core/PlayerAggregate/PlayerBase.cs
@@ -7,9 +7,24 @@ using Azul.Core.TileFactoryAggregate.Contracts;
 namespace Azul.Core.PlayerAggregate;
 
 /// <inheritdoc cref="IPlayer"/>
-internal class PlayerBase
+internal class PlayerBase : IPlayer
 {
-    protected PlayerBase(Guid id, string name, DateOnly? lastVisitToPortugal)
+    protected PlayerBase(Guid id, string name, DateOnly? lastVisitToPortugal) 
     {
+        Id = id;
+        Name = name;
+        LastVisitToPortugal = lastVisitToPortugal;
     }
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateOnly? LastVisitToPortugal { get; set; }
+
+    public IBoard Board { get; set; }
+
+    public bool HasStartingTile { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public List<TileType> TilesToPlace { get; set; }
 }
