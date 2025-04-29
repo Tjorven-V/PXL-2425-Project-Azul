812b0862e8191835fecf605fcb2fb769a0f8ac97
MuhammedColPXL <muhammed.col@student.pxl.be>
2025-04-29 18:14:57 +0200

    US5 GameTests

--- Backend/Azul.Core/GameAggregate/Game.cs
+++ Backend/Azul.Core/GameAggregate/Game.cs
@@ -8,7 +8,9 @@ namespace Azul.Core.GameAggregate;
 /// <inheritdoc cref="IGame"/>
 internal class Game : IGame
 {
-    
+    public Guid Id { get; }
+    public ITileFactory TileFactory { get; }
+    public IPlayer[] Players { get; }
 
     /// <summary>
     /// Creates a new game and determines the player to play first.
@@ -16,18 +18,44 @@ internal class Game : IGame
     /// <param name="id">The unique identifier of the game</param>
     /// <param name="tileFactory">The tile factory</param>
     /// <param name="players">The players that will play the game</param>
+
     public Game(Guid id, ITileFactory tileFactory, IPlayer[] players)
     {
-       
+        Id = id;
+        TileFactory = tileFactory;
+        Players = players;
+
+        TileFactory.TableCenter.AddStartingTile();
+
+        foreach (var player in Players)
+        {
+            player.HasStartingTile = false;
+        }
+
+        TileFactory.FillDisplays();
     }
 
-    public Guid Id => throw new NotImplementedException();
+    public Guid PlayerToPlayId
+    {
+        get
+        {
+            IPlayer player1 = Players[0];
+            IPlayer player2 = Players[1];
 
-    public ITileFactory TileFactory => throw new NotImplementedException();
+            if (!player1.LastVisitToPortugal.HasValue && !player2.LastVisitToPortugal.HasValue)
+                return player1.Id;
 
-    public IPlayer[] Players => throw new NotImplementedException();
+            if (!player1.LastVisitToPortugal.HasValue)
+                return player2.Id;
 
-    public Guid PlayerToPlayId => throw new NotImplementedException();
+            if (!player2.LastVisitToPortugal.HasValue)
+                return player1.Id;
+
+            return player1.LastVisitToPortugal > player2.LastVisitToPortugal
+                ? player1.Id
+                : player2.Id;
+        }
+    }
 
     public int RoundNumber => throw new NotImplementedException();
 
