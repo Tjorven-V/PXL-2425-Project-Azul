4a3e053083d2d11db69734e6a1e3225775fbe568
MuhammedColPXL <muhammed.col@student.pxl.be>
2025-04-29 11:04:20 +0200

    TileBagTests done

--- Backend/Azul.Core/TileFactoryAggregate/TileBag.cs
+++ Backend/Azul.Core/TileFactoryAggregate/TileBag.cs
@@ -1,24 +1,53 @@
-﻿using Azul.Core.TileFactoryAggregate.Contracts;
+﻿using System;
+using Azul.Core.TileFactoryAggregate.Contracts;
 
 namespace Azul.Core.TileFactoryAggregate;
 
 /// <inheritdoc cref="ITileBag"/>
 internal class TileBag : ITileBag
 {
-    public IReadOnlyList<TileType> Tiles => throw new NotImplementedException();
+    private readonly List<TileType> _tiles = new List<TileType>();
+    public IReadOnlyList<TileType> Tiles => _tiles;
+    private readonly Random _random = new Random();
 
     public void AddTiles(int amount, TileType tileType)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < amount; i++)
+        {
+            _tiles.Add(tileType);
+        }
     }
 
     public void AddTiles(IReadOnlyList<TileType> tilesToAdd)
     {
-        throw new NotImplementedException();
+        _tiles.AddRange(tilesToAdd);
     }
 
     public bool TryTakeTiles(int amount, out IReadOnlyList<TileType> tiles)
     {
-        throw new NotImplementedException();
+        if (amount <= 0)
+        {
+            tiles = new List<TileType>();
+            return false;
+        }
+
+        if (amount > _tiles.Count)
+        {
+            var allTiles = _tiles.ToList();
+            _tiles.Clear();
+            tiles = allTiles;
+            return false;
+        }
+
+        var shuffledTiles = _tiles.OrderBy(x => _random.Next()).ToList();
+        var takenTiles = shuffledTiles.Take(amount).ToList();
+
+        foreach (var tile in takenTiles)
+        {
+            _tiles.Remove(tile);
+        }
+
+        tiles = takenTiles;
+        return true;
     }
 }
