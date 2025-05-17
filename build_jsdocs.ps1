aceb5c02f0cf01946df521db42749a18a7dd8d7c
TjorvenVannieuwenhuysePXL <tjorven.vannieuwenhuyse@student.pxl.be>
2025-05-17 12:47:28 +0200

    delete unused jsdocs

--- build_jsdocs.ps1
+++
@@ -1,8 +0,0 @@
-clear; Remove-Item -Recurse -Force "Documentation" -ErrorAction SilentlyContinue
-
-# npx will fetch jsdoc remotely instead of downloading it, preventing cluttering the project with node_modules files
-npx jsdoc -c jsdocs.json
-
-echo Finished, opening...
-
-start .\Documentation\index.html
